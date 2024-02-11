d2ad1f38c6203c465b61b1eee4a30a746886dbc1
Ricardo E. Sanchez <ricardose0419@gmail.com>
2024-02-10 18:59:02 -0600

    set up app.js added a NotFound and general error handler

--- src/app.js
+++ src/app.js
@@ -1,5 +1,31 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+// Not Found Error Handler
+app.use((req, res, next) => {
+    next({ status: 404, message: "Not Found" });
+  });
+  
+  // General Error Handler
+  app.use((err, req, res, next) => {
+    const { status = 500, message = "Something went wrong!" } = err;
+    res.status(status).json({ error: message });
+  });
+
 
 module.exports = app;
