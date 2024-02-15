d647c3a7b2828859a3663840df00c83921d1b323
Ricardo E. Sanchez <ricardose0419@gmail.com>
2024-02-15 10:50:51 -0600

    modified movie-theaters migration file

--- src/db/migrations/20240210175643_movies_theaters.js
+++ src/db/migrations/20240210175643_movies_theaters.js
@@ -1,16 +1,22 @@
-exports.up = function(knex) {
-    return knex.schema.createTable('movies_theaters', (table) => {
-      table.integer('movie_id').unsigned().notNullable(); // ID of the movie
-      table.foreign('movie_id').references('movie_id').inTable('movies'); // Foreign key to movies table
-      table.integer('theater_id').unsigned().notNullable(); // ID of the theater
-      table.foreign('theater_id').references('theater_id').inTable('theaters'); // Foreign key to theaters table
-      table.boolean('is_showing').notNullable().defaultTo(false); // Whether the movie is currently showing in the theater
-      table.primary(['movie_id', 'theater_id']); // Composite primary key
-      table.timestamps(true, true)
-    });
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('movies_theaters'); // Rollback migration
-  };
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("movies_theaters", (table) => {
+    table.integer("movie_id").unsigned().notNullable();
+    table
+      .foreign("movie_id")
+      .references("movie_id")
+      .inTable("movies")
+      .onDelete("CASCADE");
+    table.integer("theater_id").unsigned().notNullable();
+    table
+      .foreign("theater_id")
+      .references("theater_id")
+      .inTable("theaters")
+      .onDelete("CASCADE");
+    table.boolean("is_showing");
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("movies_theaters");
+};
