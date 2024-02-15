72d2be47633701fca47637a570d89866e7edc9f1
Ricardo E. Sanchez <ricardose0419@gmail.com>
2024-02-15 13:27:21 -0600

    removed duplicate defitions

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -13,29 +13,6 @@ async function getMovie(req, res) {
   res.json({ data: movie });
 }
 
-// Middleware to check if a movie exists
-async function movieExists(req, res, next) {
-  const { movieId } = req.params;
-  const movie = await MoviesService.read(movieId);
-
-  if (movie) {
-    res.locals.movie = movie; // Store the found movie in the response locals for use in the next handler
-    return next(); // Proceed to the next middleware/route handler
-  }
-
-  // If the movie is not found, return a 404 error
-  return next({
-    status: 404,
-    message: `Movie cannot be found with id: ${movieId}.`,
-  });
-}
-
-async function getMovie(req, res) {
-  // If movieExists middleware has already found the movie, it's available in res.locals.movie
-  res.json({ data: res.locals.movie });
-}
-
-
 
 async function movieExists(req, res, next) {
     const { movieId } = req.params;
